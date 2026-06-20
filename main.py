1143e689a5223c87433d5ff02b1ef3e0c783c442
burneybuilds <tusharburney@gmail.com>
2026-06-20 17:56:13 +0530

    feat: add random pokemon and input validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
-import os 
+import os
+import random 
 
 
 def cli_art(): 
@@ -26,7 +27,6 @@ def about_me():
         print("Thanks for checking it out!")
         print("==========================================================================================")
 
-        
         input("\nPress Enter to return to the menu...")
         clear_screen() # Clear the screen after displaying.
 
@@ -35,39 +35,82 @@ def clear_screen(): #This Function Clear the screen.
     os.system("cls" if os.name == "nt" else "clear") # I have no idea what it does. 
 
 
-def output(): # This function takes the response from the API and displays it in a nice format.
-    while True: #Runs a infinite loop until the user enters "1" to exit.
-        response = get_pokemon() # takes the output from the fuction get pokemon and stores it in response.
-        if response == "Exit": # checks if it is exit then break the fucntion and return to the main menu.
+# @ : This function takes the response from the API and displays it in a nice format.
+def output(): 
+    while True: 
+        response = get_pokemon()
+        if response == "Exit": 
+            clear_screen() 
             break
-        else: # or show the ouput in a nice format.
+        elif response == "Data Not Found": 
+            print("No Pokemon with such name found. Please try again.") 
+        elif response == "Number":
+            print("Invalid Input.")
+        else: # show the ouput in a nice format.
+            id = response["id"]
             name = response["name"]
             height = response["height"]
             type_name = response["types"][0]["type"]["name"]
             weight = response["weight"]
+            #history = search_histoy()
             clear_screen()
+            cli_art()
             print("                                        ")
             print("========================================")
-            print(f"Name: {name}")
-            print(f"Type: {type_name}")
-            print(f"Height: {height}")
-            print(f"Weight: {weight}")
+            print(f"| ID: {id}                             ")
+            print(f"| Name: {name}                         ")    
+            print(f"| Type: {type_name}                    ")
+            print(f"| Height: {height}                     ")
+            print(f"| Weight: {weight}                     ")
             print("========================================")
-            print("                                        ")#Runs a infinite loop until the user enters "1" to exit.
+            print("                                        ")
+            #print(history)
 
 def get_pokemon():
     while True: #Runs a infinite loop until the user enters "1" to exit.
         pokemon = input("Enter the Name of the Pokemon: ").strip().title()  # Takes input and stores it in pokemon.
         if pokemon == "1":
-            return "Exit" 
+            return "Exit"
+        elif pokemon.isdigit():
+            return "Number" 
         else:
             url = requests.get("https://pokeapi.co/api/v2/pokemon/")
             if url.status_code == 200: # check if the api end is working or not.
                 pokemon_name = pokemon 
                 request_url = "https://pokeapi.co/api/v2/pokemon/" + pokemon_name
                 response = requests.get(request_url) # just for debugging
-                pokemon_data = response.json()  # Get the data from the api in the json format.
-                return pokemon_data #Sends the json data to bridge fucntion to display the data in a nice format.
+                if response.status_code ==  404:
+                    return "Data Not Found"
+                else:
+                    pokemon_data = response.json()  # Get the data from the api in the json format.
+                    return pokemon_data #Sends the json data to bridge fucntion to display the data in a nice format.
+
+
+def random_pokemon():
+    pokemon_id = random.randint(1, 1025)
+    pokemon_id =str(pokemon_id)
+    request_url = "https://pokeapi.co/api/v2/pokemon/" + pokemon_id
+    response = requests.get(request_url)
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        index_id = pokemon_data["id"]
+        name = pokemon_data["name"]
+        height = pokemon_data["height"]
+        type_name = pokemon_data["types"][0]["type"]["name"]
+        weight = pokemon_data["weight"]
+        clear_screen()
+        cli_art()
+        print("                                        ")
+        print("========================================")
+        print(f"| ID: {index_id}                       ")
+        print(f"| Name: {name}                         ")    
+        print(f"| Type: {type_name}                    ")
+        print(f"| Height: {height}                     ")
+        print(f"| Weight: {weight}                     ")
+        print("========================================")
+        print("                                        ")
+    elif response.status_code == 404:
+        return "Connection Time-Out."
 
 
 def search_menu():
@@ -75,36 +118,41 @@ def search_menu():
         cli_art() # You can remove this if you want. 
         print("=======================")
         print("1. Search   ") 
-        print("2. About Me ") 
+        print("2. Random Pokemon ")
+        print("3. About Me ") 
         print("3. Exit     ")
         print("=======================")
         user_choice = input("Enter your Choice: ") #Takes Input and stores it in user_choice.
         if user_choice == "1":  # Currently, 1 is string not a int and so the other.
-            output() # Call a fucntion call Output() around line 38.
+            output()
         elif user_choice == "2":
-            about_me() # This is fucntion, just to let you know who made it. 
+            random_pokemon()
         elif user_choice == "3":
+            about_me() 
+        elif user_choice == "4":
+            clear_screen()
             break
         else:
             print("Invalid Choice")
 
+# TODO: Need to make this recent history feature. 
+# ! Not Completed
+def search_histoy():
+    store = 5 
+    for _ in store:
+        history = []
+        _ , name = get_pokemon()
+        if name == "Exit" or name == "Number" or name == "404":
+            continue
+        else:
+            history.append(name)
+    recent_history = f"Recent: {history}"
+    return recent_history
+
 def main():
+    # Clear the screen before runing the program
+    clear_screen()
     search_menu()
-    # user_choice = input("Enter your Choice: ") #Takes Input and stores it in user_choice
-    # if user_choice.isdigit():
-    #     search_menu(user_choice)
-    # else:
-    #     print("Invalid Choice")
 
 if __name__ == "__main__":
     main()
-
-
-"""
-Data Flow: 
-
-
-
-
-
-"""
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Poct-Dex
